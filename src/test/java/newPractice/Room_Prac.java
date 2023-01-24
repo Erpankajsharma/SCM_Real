d23499881cad8dda34af8f911a641937416078e4
Pankaj <pankajsharma5621@gmail.com>
2023-01-24 22:19:11 +0530

    practicing at room

---
+++ src/test/java/newPractice/Room_Prac.java
@@ -0,0 +1,13 @@
+package newPractice;
+
+import org.testng.annotations.Test;
+
+public class Room_Prac {
+	
+	@Test
+	public void roomPrac()
+	{
+		System.out.println("first practice at room.");
+	}
+
+}
